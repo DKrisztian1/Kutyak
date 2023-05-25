f97d796872b08f44f14a9f75a15b53c305103817
DKrisztian1 <dkrisztian2005@gmail.com>
2023-05-25 11:48:55 +0200

    4. ora vege

---
+++ ConsoleApp5/Datas/Kutyak.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace KutyaClass
+{
+    internal class KutyaNevek
+    {
+        int kutyaNevId;
+        string kutyaNev;
+
+        public KutyaNevek(int kutyaNevId, string kutyaNev)
+        {
+            this.kutyaNevId = kutyaNevId;
+            this.kutyaNev = kutyaNev;
+        }
+
+        public int KutyaNevId { get => kutyaNevId; }
+        public string KutyaNev { get => kutyaNev; }
+    }
+
+    internal class KutyaFajtak
+    {
+        int kutyaFajId;
+        string kutyaFajNev;
+        string kutyaEredetiFajNev;
+
+        public KutyaFajtak(int kutyaFajId, string kutyaFajNev, string kutyaEredetiFajNev)
+        {
+            this.kutyaFajId = kutyaFajId;
+            this.kutyaFajNev = kutyaFajNev;
+            this.kutyaEredetiFajNev = kutyaEredetiFajNev;
+        }
+
+        public int KutyaFajId { get => kutyaFajId; }
+        public string KutyaFajNev { get => kutyaFajNev; }
+        public string KutyaEredetiFajNev { get => kutyaEredetiFajNev; }
+    }
+
+    internal class Kutyak
+    {
+        int kutyaId;
+        int fajid;
+        int nevId;
+        int eletkor;
+        string utsOrvosiEll;
+        public Kutyak(int kutyaId, int fajid, int nevId, int eletkor, string utsOrvosiEll)
+        {
+            this.kutyaId = kutyaId;
+            this.fajid = fajid;
+            this.nevId = nevId;
+            this.eletkor = eletkor;
+            this.utsOrvosiEll = utsOrvosiEll;
+        }
+
+        public int KutyaId { get => kutyaId; }
+        public int Fajid { get => fajid; }
+        public int NevId { get => nevId; }
+        public int Eletkor { get => eletkor; }
+        public string UtsOrvosiEll { get => utsOrvosiEll; }
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KutyaClass;
+using System.IO;
+using System.Linq;
+
+List<KutyaNevek> kutyaNevekLista = new List<KutyaNevek>();
+List<KutyaFajtak> kutyaFajtakLista = new List<KutyaFajtak>();
+List<Kutyak> kutyakLista = new List<Kutyak>();
+
+StreamReader nevekReader = new StreamReader("KutyaNevek.csv");
+while (!nevekReader.EndOfStream)
+{
+    string[] sor = nevekReader.ReadLine().Split(";");
+    KutyaNevek ujKutyaNev = new KutyaNevek(Convert.ToInt32(sor[0]), sor[1]);
+    kutyaNevekLista.Add(ujKutyaNev);
+}
+nevekReader.Close();
+
+StreamReader fajtakReader = new StreamReader("KutyaFajtak.csv");
+while (!nevekReader.EndOfStream)
+{
+    string[] sor = fajtakReader.ReadLine().Split(";");
+    KutyaFajtak ujKutyaFajta = new KutyaFajtak(Convert.ToInt32(sor[0]), sor[1], sor[2]);
+    kutyaFajtakLista.Add(ujKutyaFajta);
+}
+fajtakReader.Close();
